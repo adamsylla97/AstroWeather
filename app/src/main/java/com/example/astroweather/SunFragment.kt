f52b6375f6097db1891df82afe6cc5bd8a723f64
adamsylla97 <209474@edu.p.lodz.pl>
2019-04-24 19:56:09 +0200

    *added inicialization of text views for sun fragment *added static set of text views for sun fragment

--- app/src/main/java/com/example/astroweather/SunFragment.kt
+++ app/src/main/java/com/example/astroweather/SunFragment.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.astrocalculator.AstroCalculator
 import com.astrocalculator.AstroDateTime
 
@@ -23,17 +24,30 @@ private const val ARG_PARAM2 = "param2"
 class SunFragment : Fragment() {
 
     lateinit var fragmentView: View
+    lateinit var sunRise: TextView
+    lateinit var sunSet: TextView
+    lateinit var sunCivRise: TextView
+    lateinit var sunCivSet: TextView
+
+    fun initTextViews(){
+        sunRise = fragmentView.findViewById(R.id.sunRise)
+        sunSet = fragmentView.findViewById(R.id.sunSet)
+        sunCivRise = fragmentView.findViewById(R.id.sunCivRise)
+        sunCivSet = fragmentView.findViewById(R.id.sunCivSet)
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         fragmentView = inflater.inflate(R.layout.fragment_sun, container, false)
 
+        initTextViews()
+
         lateinit var astroCalculatorLocation: AstroCalculator.Location
         astroCalculatorLocation = AstroCalculator.Location(52.14,21.01)
 
         val astroDateTime = AstroDateTime()
-        astroDateTime.day = 31
-        astroDateTime.month = 1
+        astroDateTime.day = 24
+        astroDateTime.month = 12
         astroDateTime.year = 2019
 
         val astroCalculator = AstroCalculator(astroDateTime,astroCalculatorLocation)
@@ -41,6 +55,17 @@ class SunFragment : Fragment() {
         Log.i("AstroCalc SUN SET",astroCalculator.sunInfo.sunset.toString())
         Log.i("AstroCalc CIV SUN RISE",astroCalculator.sunInfo.twilightMorning.toString())
         Log.i("AstroCalc CIV SUN SET",astroCalculator.sunInfo.twilightEvening.toString())
+
+        var temp: List<String>? = null
+        temp = astroCalculator.sunInfo.sunrise.toString().split(" ")
+        sunRise.text = temp[1]
+        temp = astroCalculator.sunInfo.sunset.toString().split(" ")
+        sunSet.text = temp[1]
+        temp = astroCalculator.sunInfo.twilightMorning.toString().split(" ")
+        sunCivRise.text = temp[1]
+        temp = astroCalculator.sunInfo.twilightEvening.toString().split(" ")
+        sunCivSet.text = temp[1]
+
         return fragmentView
     }
 
