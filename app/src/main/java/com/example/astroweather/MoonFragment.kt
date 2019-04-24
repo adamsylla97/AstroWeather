b329d5754900d57309111ee0f44059827fd3433c
adamsylla97 <209474@edu.p.lodz.pl>
2019-04-24 21:39:19 +0200

    *added moon information in logs *fixed moonfragment.kt (loading proper layout now)

--- app/src/main/java/com/example/astroweather/MoonFragment.kt
+++ app/src/main/java/com/example/astroweather/MoonFragment.kt
@@ -3,25 +3,44 @@ package com.example.astroweather
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import com.astrocalculator.AstroCalculator
+import com.astrocalculator.AstroDateTime
 
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- *
- */
 class MoonFragment : Fragment() {
 
+    lateinit var fragmentView: View
+
+    fun initTextViews(){
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_moon, container, false)
+        fragmentView = inflater.inflate(R.layout.fragment_moon, container, false)
+
+        initTextViews()
+
+        lateinit var astroCalculatorLocation: AstroCalculator.Location
+        astroCalculatorLocation = AstroCalculator.Location(52.14,21.01)
+
+        val astroDateTime = AstroDateTime(2019,4,24,20,43,11,1,true)
+        astroDateTime.day = 24
+        astroDateTime.month = 4
+        astroDateTime.year = 2019
+
+        val astroCalculator = AstroCalculator(astroDateTime,astroCalculatorLocation)
+        Log.i("AstroCalc MOON",astroCalculator.moonInfo.moonrise.toString())
+        Log.i("AstroCalc MOON",astroCalculator.moonInfo.moonset.toString())
+        Log.i("AstroCalc MOON",astroCalculator.moonInfo.nextNewMoon.toString())
+        Log.i("AstroCalc MOON",astroCalculator.moonInfo.nextFullMoon.toString())
+        Log.i("AstroCalc MOON",astroCalculator.moonInfo.illumination.toString())
+
+
+        return fragmentView
     }
 
 
